a6c9d846ce05f2a46a9f63f0e0713b3ff12441f9
Joseph <josephmdemarco04@gmail.com>
2025-01-17 14:43:50 -0500

    added age to string input

--- intro/Week2/String_Input.c
+++ intro/Week2/String_Input.c
@@ -3,9 +3,14 @@
 int main(){
     char firstName[30];
     char lastName[30];
+    int age;
     printf("What is your first name? ");
     scanf("%s", firstName);
     printf("What is your last name? ");
     scanf("%s", lastName);
+    printf("How old are you? ");
+    scanf("%d", &age);
     printf("Hello, %s %s!\n", firstName, lastName);
+    printf("You are %d years old.\n", age);
+    printf("Memory address of your age: %p\n", &age);
 }
