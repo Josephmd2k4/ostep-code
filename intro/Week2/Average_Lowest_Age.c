1e58cab57d2a00c6d6b78b12bebb1c832592e970
Joseph <josephmdemarco04@gmail.com>
2025-01-17 14:29:59 -0500

    fixed array exercise

--- intro/Week2/Average_Lowest_Age.c
+++ intro/Week2/Average_Lowest_Age.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int theAges[10];
+    int amount;
+
+    printf("How many people are you going to enter the age for? ");
+    scanf("%d", &amount);
+
+    int theAges[amount];
     int sum = 0;
     int min = 999;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < amount; i++){
         printf("Enter the age of person %d: ", i + 1);
         scanf("%d", &theAges[i]);
         sum += theAges[i];
@@ -13,7 +18,7 @@ int main(){
         }
     }
     //print the average age
-    float average = sum / 10.0;
+    float average = sum / amount;
     printf("The average age is: %.1f\n", average);
 
     //print the smallest age
